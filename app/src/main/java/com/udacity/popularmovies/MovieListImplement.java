0f82608341ae0c3aff236c4c623af21d7877480f
MrinaliniPal <minupal95@gmail.com>
2016-06-03 22:28:43 +0530

    Detail.java not completed

---
+++ app/src/main/java/com/udacity/popularmovies/MovieListImplement.java
@@ -0,0 +1,38 @@
+package com.udacity.popularmovies;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by Mrinalini galle on 03-06-2016.
+ */
+public class MovieListImplement extends ArrayAdapter<String> {
+
+    private final Activity activity;
+    private final String[] moviePoster;
+
+
+    MovieListImplement(Activity activity,String[] moviePoster) {
+        super(activity,R.layout.single_poster,moviePoster);
+        this.activity=activity;
+        this.moviePoster=moviePoster;
+    }
+
+    public View getView(int i, View view, ViewGroup viewGroup)
+    {
+        LayoutInflater inflater = activity.getLayoutInflater();
+        final View rowView = inflater.inflate(R.layout.single_poster,null,true);
+
+        ImageView imageView = (ImageView)view.findViewById(R.id.singlePoster);
+        Picasso.with(activity).load(moviePoster[i]).into(imageView);
+
+        return rowView;
+    }
+}
--- app/src/main/java/com/udacity/popularmovies/MoviePosterFragment.java
+++ app/src/main/java/com/udacity/popularmovies/MoviePosterFragment.java
@@ -8,15 +8,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MoviePosterFragment extends Fragment {
 
     RequestQueue requestQueue;
-    String[] movieposters;
+    String[] moviePosters;
     GridView gridView;
 
     @Override
@@ -41,5 +51,44 @@ public class MoviePosterFragment extends Fragment {
     public void movieDetailReceive(final String parturl)
     {
         String mainurl = Utils.BASE_MOVIE_URL+parturl+Utils.API_KEY;
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, mainurl, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray movieDetail = response.getJSONArray("results");
+                    moviePosters = new String[movieDetail.length()];
+                    final String[] movieID = new String[movieDetail.length()];
+                    final String[] title = new String[movieDetail.length()];
+
+                    for(int i=0;i<movieDetail.length();i++)
+                    {
+                        movieID[i]=movieDetail.getJSONObject(i).getString("id");
+                        title[i]=movieDetail.getJSONObject(i).getString("title");
+                        moviePosters[i]=Utils.BASE_PICTURE_URL+Utils.PICTURE_SIZE1+movieDetail.getJSONObject(i).getString("poster_path");
+
+                        MovieListImplement adapter = new MovieListImplement(getActivity(),moviePosters);
+                        gridView.setAdapter(adapter);
+                        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                if(MainActivity.isSinglePane)
+                                {
+
+                                }
+                            }
+                        });
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(getActivity(), "Error", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                Toast.makeText(getActivity(), "Internet not working", Toast.LENGTH_SHORT).show();
+            }
+    });
     }
 }
